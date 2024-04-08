8b63a002ad380fe5c6ff71f4bee154a58f351f04
Boris Ionine <boris.ionine@multiviewcorp.com>
2024-04-08 14:37:30 -0400

    removing need for file name, added support for "latest" tag

--- RunBootstrapSSMDoc.ps1
+++ RunBootstrapSSMDoc.ps1
@@ -4,8 +4,6 @@ param(
     [Parameter(Mandatory=$true)]
     [string]$accountID,
     [Parameter(Mandatory=$true)]
-    [string]$fileName,
-    [Parameter(Mandatory=$true)]
     [string]$domainOwner,
     [Parameter(Mandatory=$true)]
     [string]$nameSpace,
@@ -28,8 +26,8 @@ aws --version
 
 $targetTag = "Name"
 
-Write-Output "aws ssm send-command --targets Key=tag:$targetTag,Values=$vmName --document-name arn:aws:ssm:us-east-1:$($accountID):document/Bootstrap --parameters ""FileName=$fileName, DomainOwner=$domainOwner, NameSpace=$nameSpace, Package=$package, PackageVersion=$packageVersion, Repository=$repository, ScriptToRun=$scriptToRun, DelimitedScriptParameters=$delimitedScriptParameters"" --region us-east-1"
-$result = aws ssm send-command --targets "Key=tag:$targetTag,Values=$vmName" --document-name arn:aws:ssm:us-east-1:$($accountID):document/Bootstrap --parameters "FileName=$fileName, DomainOwner=$domainOwner, NameSpace=$nameSpace, Package=$package, PackageVersion=$packageVersion, Repository=$repository, ScriptToRun=$scriptToRun, DelimitedScriptParameters=$delimitedScriptParameters" --region us-east-1 | ConvertFrom-Json
+Write-Output "aws ssm send-command --targets Key=tag:$targetTag,Values=$vmName --document-name arn:aws:ssm:us-east-1:$($accountID):document/Bootstrap --parameters ""DomainOwner=$domainOwner, NameSpace=$nameSpace, Package=$package, PackageVersion=$packageVersion, Repository=$repository, ScriptToRun=$scriptToRun, DelimitedScriptParameters=$delimitedScriptParameters"" --region us-east-1"
+$result = aws ssm send-command --targets "Key=tag:$targetTag,Values=$vmName" --document-name arn:aws:ssm:us-east-1:$($accountID):document/Bootstrap --parameters "DomainOwner=$domainOwner, NameSpace=$nameSpace, Package=$package, PackageVersion=$packageVersion, Repository=$repository, ScriptToRun=$scriptToRun, DelimitedScriptParameters=$delimitedScriptParameters" --region us-east-1 | ConvertFrom-Json
 
 Write-Output "Command created at: https://us-east-1.console.aws.amazon.com/systems-manager/run-command/$($result.Command.CommandId)?region=us-east-1"
 Write-Output "CommandId is: $($result.Command.CommandId)"
