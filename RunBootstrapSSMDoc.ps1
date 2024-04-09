dddd64b943d2246705036e8516dbb77590c948cb
Boris Ionine <boris.ionine@multiviewcorp.com>
2024-04-09 13:04:52 -0400

    share SSM doc from Multiview1

--- RunBootstrapSSMDoc.ps1
+++ RunBootstrapSSMDoc.ps1
@@ -2,8 +2,6 @@ param(
     [Parameter(Mandatory=$true)]
     [string]$vmName,
     [Parameter(Mandatory=$true)]
-    [string]$accountID,
-    [Parameter(Mandatory=$true)]
     [string]$domainOwner,
     [Parameter(Mandatory=$true)]
     [string]$nameSpace,
@@ -26,8 +24,8 @@ aws --version
 
 $targetTag = "Name"
 
-Write-Output "aws ssm send-command --targets Key=tag:$targetTag,Values=$vmName --document-name arn:aws:ssm:us-east-1:$($accountID):document/Bootstrap --parameters ""DomainOwner=$domainOwner, NameSpace=$nameSpace, Package=$package, PackageVersion=$packageVersion, Repository=$repository, ScriptToRun=$scriptToRun, DelimitedScriptParameters=$delimitedScriptParameters"" --region us-east-1"
-$result = aws ssm send-command --targets "Key=tag:$targetTag,Values=$vmName" --document-name arn:aws:ssm:us-east-1:$($accountID):document/Bootstrap --parameters "DomainOwner=$domainOwner, NameSpace=$nameSpace, Package=$package, PackageVersion=$packageVersion, Repository=$repository, ScriptToRun=$scriptToRun, DelimitedScriptParameters=$delimitedScriptParameters" --region us-east-1 | ConvertFrom-Json
+Write-Output "aws ssm send-command --targets Key=tag:$targetTag,Values=$vmName --document-name arn:aws:ssm:us-east-1:$($domainOwner):document/Bootstrap --parameters ""DomainOwner=$domainOwner, NameSpace=$nameSpace, Package=$package, PackageVersion=$packageVersion, Repository=$repository, ScriptToRun=$scriptToRun, DelimitedScriptParameters=$delimitedScriptParameters"" --region us-east-1"
+$result = aws ssm send-command --targets "Key=tag:$targetTag,Values=$vmName" --document-name arn:aws:ssm:us-east-1:$($domainOwner):document/Bootstrap --parameters "DomainOwner=$domainOwner, NameSpace=$nameSpace, Package=$package, PackageVersion=$packageVersion, Repository=$repository, ScriptToRun=$scriptToRun, DelimitedScriptParameters=$delimitedScriptParameters" --region us-east-1 | ConvertFrom-Json
 
 Write-Output "Command created at: https://us-east-1.console.aws.amazon.com/systems-manager/run-command/$($result.Command.CommandId)?region=us-east-1"
 Write-Output "CommandId is: $($result.Command.CommandId)"
